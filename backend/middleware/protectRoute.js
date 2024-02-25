94110e07610503924fb35f625a3e9ac6df36e29e
avengsum <sy710481@gmail.com>
2024-02-26 01:53:36 +0530

    Protectroute done

---
+++ backend/middleware/protectRoute.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/userModel';
+
+const protectRoute = async (req,res,next) => {
+  try {
+    const token = req.cookies.jwt;
+
+    if(!token){
+      return res.status(401).json({error:"Unauthorized - No Token Provided"})
+    }
+
+    const decode = jwt.verify(token,process.env.SECRET)
+
+    if(!decode){
+      return res.status(401).json({error:"Unauthorized - invalid token"})
+    }
+
+    const user = await User.findOne(decode.userId).select("-password");
+
+    req.user = user;
+
+    next();
+
+  } catch (error) {
+    console.log("Error in protectRoute controller",error.message)
+    res.status(500).json({error:"Internal Server Error"})
+  }
+}
+
+export default protectRoute;
